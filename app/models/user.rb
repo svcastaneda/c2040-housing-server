14dcd28320c036e1585e9e01f397f679fdfb25a0
svcastaneda <svcastaneda@gmail.com>
2017-07-15 16:24:11 -0700

    Add location validation #AnothaOne

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  has_one :location
+  belongs_to :location
   has_many :work_items
   
   validates_uniqueness_of :email
-  validates_presence_of :fname, :lname, :email, :password
+  validates_presence_of :fname, :lname, :email, :password, :location_id
   validates :password, presence: true, allow_nil: true, length: {minimum: 8}
 end
