3de0712f9238b9f9eb5eea1a7420b6e563ef1716
svcastaneda <svcastaneda@gmail.com>
2017-07-15 15:13:48 -0700

    Add user validations and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  has_one :location
+  has_many :work_items
+  
+  validates_uniqueness_of :email
+  validates_presence_of :fname, :lname, :email, :password
+  validates :password, presence: true, allow_nil: true, length: {minimum: 8}
 end
