d9443d6516ff8103419fed3a80444e9ffae2a972
Alex Garcia <alexsebastian.garcia+code2040@gmail.com>
2017-07-16 01:43:09 +0000

    lol my bad

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 require 'dotenv'
 Dotenv.load
 
-puts ENV["ADSF_TEST"]
+#puts ENV["ADSF_TEST"]
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
