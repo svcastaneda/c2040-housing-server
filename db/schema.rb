6a2bec1ba516154ef2a34c8333f9db01b10af17a
svcastaneda <svcastaneda@gmail.com>
2017-07-15 19:53:17 -0700

    Remove passwords to make post requests work #hackathon 😂

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715222447) do
+ActiveRecord::Schema.define(version: 20170716024420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,14 +25,13 @@ ActiveRecord::Schema.define(version: 20170715222447) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "fname",           null: false
-    t.string   "lname",           null: false
-    t.string   "email",           null: false
+    t.string   "fname",       null: false
+    t.string   "lname",       null: false
+    t.string   "email",       null: false
     t.string   "phone"
-    t.integer  "location_id",     null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "location_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "users", ["location_id"], name: "index_users_on_location_id", using: :btree
