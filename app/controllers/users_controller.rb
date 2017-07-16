92dbe40581a4010725325ce1763d41407c74e10e
svcastaneda <svcastaneda@gmail.com>
2017-07-15 19:54:31 -0700

    Delete all remaints of password_digest 💀

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_action :verify_authentication_token
+  # skip_before_action :verify_authentication_token
 
   # GET /users
   # GET /users.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  
   belongs_to :location
   has_many :work_items
   
   validates_uniqueness_of :email
-  validates_presence_of :fname, :lname, :email, :password, :location_id
-  validates :password, presence: true, allow_nil: true, length: {minimum: 8}
+  validates_presence_of :fname, :lname, :email, :location_id
 end
